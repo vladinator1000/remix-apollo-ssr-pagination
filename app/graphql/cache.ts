e3b233e093172955da60b6ccd1b1a6ae71de67bf
Vlady Veselinov <vladkodmc@gmail.com>
2024-05-21 14:53:31 +0100

    add doc

--- app/graphql/cache.ts
+++ app/graphql/cache.ts
@@ -1,6 +1,9 @@
 import { InMemoryCache } from '@apollo/client/core/index.js'
 import { relayStylePagination } from '@apollo/client/utilities/index.js'
 
+/**
+ * The trick is to have the same cache both on the server and the client.
+ */
 export function createCache() {
   let cache = new InMemoryCache({
     possibleTypes: { Profile: ['Artist', 'Venue', 'Promoter', 'User'] },
